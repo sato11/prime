9db9b16bbf3cd23f7aadfb8055167a236d747c22
Junichi Sato <22004610+sato11@users.noreply.github.com>
2022-02-05 01:30:29 +0900

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Prime.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "Prime is the fast-enough prime number tester and generator. Written in Elixir with no dependency."
   @source_url "https://github.com/sato11/prime"
 
